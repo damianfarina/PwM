92c86b3622418e0b8c5c1c61ade7056c71956b53
Damian Farina <damianfarina@gmail.com>
2017-01-07 14:24:26 -0300

    Migrate to Gradle

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-PWMaker
-=======
+PWM
+===
 
 Yet another simple password generator for android
 
-It allows to generate passwords based on a master password hardcoded in MainActivity.java and a variable keyword the user must type, these keywords combined and MD5'd generates the password
+It allows to generate passwords based on a master password hardcoded in PasswordFormActivity.java and a variable keyword the user must type, these keywords combined and MD5'd generates the password
 
 Two passwords are generated, click one or another to copy it to the clipboard
 
 Pro tip
 -------
 
-Assign a shortcut to pwmaker for quick access whenever you need it
+Assign a shortcut to PwM for quick access whenever you need it
 
 
 Warning!
 --------
 
-If you are planning to use it, just remember to change the master passwords in MainActivity.java
+If you are planning to use it, just remember to change the master passwords in PasswordFormActivity.java
---
+++ app/src/main/java/com/damianfarina/pwm/PasswordFormActivity.java
@@ -0,0 +1,109 @@
+package com.damianfarina.pwm;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PasswordFormActivity extends AppCompatActivity {
+    private ListView passwordsListView;
+    EditText keywordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        passwordsListView = (ListView) findViewById(R.id.passwords_list);
+        keywordInput = (EditText) findViewById(R.id.keyword_input);
+
+        Button generateButton = (Button) findViewById(R.id.button_generate);
+
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onGenerateButtonClick();
+            }
+        });
+
+        passwordsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String password = (String) parent.getItemAtPosition(position);
+                ClipboardManager clipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("PwM generated", password);
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+    }
+
+
+    private void onGenerateButtonClick() {
+        List<String> passwordsList = new ArrayList<>();
+
+        passwordsList.add(getMD5(keywordInput.getText().toString().trim() + _MASTER_PASS_1_).substring(0, 8));
+        passwordsList.add(getMD5(keywordInput.getText().toString().trim() + _MASTER_PASS_2_).substring(0, 8));
+        passwordsList.add(getMD5(keywordInput.getText().toString().trim() + _MASTER_PASS_3_).substring(0, 8));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                passwordsList );
+
+        passwordsListView.setAdapter(arrayAdapter);
+    }
+
+    private String getMD5(String s) {
+        MessageDigest m = null;
+
+        try {
+            m = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        m.update(s.getBytes(), 0, s.length());
+        String hash = new BigInteger(1, m.digest()).toString(16);
+
+        return hash;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_password_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
